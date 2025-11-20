c5f166b795aece095a3f20fdde6eecd157375b4b
vetchayateesh <138463611+vetchayateesh@users.noreply.github.com>
2025-11-20 09:44:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/Yateesh-Portfolio/",
+//   optimizeDeps: {
+//     exclude: ["lucide-react"],
+//   },
+// });
+
 export default defineConfig({
-  plugins: [react()],
-  base: "/Yateesh-Portfolio/",
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
+  base: "./",
 });
